d7c36054f811ccf2b71894ce8fd1daa90cfa5d7d
ugyen-lekzin <ugyen.lekzin@selise.ch>
2022-09-21 06:07:32 -0600

    added viewport

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
 
   video: true,
-  
+  viewportHeight: 1909,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
